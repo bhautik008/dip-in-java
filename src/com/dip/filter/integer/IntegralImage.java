8a0fcfd748cd27a30f16b61f89d46daf25a4e0cc
Bhautik Bhanani <patel.bhautik008@gmail.com>
2017-11-14 20:17:56 -0500

    11 - 14 changes one file

--- src/com/dip/filter/integer/IntegralImage.java
+++ src/com/dip/filter/integer/IntegralImage.java
@@ -9,89 +9,46 @@ protected int[][] integralImage = null;
     private int width;
     private int height;
 
-    /**
-     * Initializes a new instance of the IntegralImage class.
-     * @param fastBitmap An image to be processed.
-     */
-    public IntegralImage(MakeImage fastBitmap) {
-        this(fastBitmap, 1);
+    public IntegralImage(MakeImage makeImage) {
+        this(makeImage, 1);
     }
     
-    /**
-     * Initializes a new instance of the IntegralImage class.
-     * @param fastBitmap An image to be processed.
-     * @param power Power of the number.
-     */
-    public IntegralImage(MakeImage fastBitmap, int power) {
-        this.width = fastBitmap.getWidth();
-        this.height = fastBitmap.getHeight();
-        Process(fastBitmap, power);
+    public IntegralImage(MakeImage makeImage, int power) {
+        this.width = makeImage.getWidth();
+        this.height = makeImage.getHeight();
+        Process(makeImage, power);
     }
     
-    /**
-     * Initializes a new instance of the IntegralImage class.
-     * @param width Image width.
-     * @param height Image height.
-     */
     protected IntegralImage(int width, int height){
         this.width = width;
         this.height = height;
         this.integralImage = new int[height + 1][width  + 1];
     }
 
-    /**
-     * Width of the source image the integral image was constructed for.
-     * @return Width.
-     */
     public int getWidth() {
         return width;
     }
 
-    /**
-     * Height of the source image the integral image was constructed for.
-     * @return Height.
-     */
     public int getHeight() {
         return height;
     }
     
-    /**
-     * Provides access to internal array keeping integral image data.
-     * @return Data.
-     */
     public int[][] getInternalData(){
         return integralImage;
     }
     
-    /**
-     * Provides access to internal value keeping integral image data.
-     * @param x X axis coordinate.
-     * @param y Y axis coordinate.
-     * @return Integral value.
-     */
     public int getInternalData(int x, int y){
         return integralImage[x][y];
     }
     
-    /**
-     * Construct integral image from source grayscale image.
-     * @param fastBitmap Image to be processed.
-     * @return Returns integral image.
-     */
-    public static IntegralImage FromFastBitmap(MakeImage fastBitmap){
-        return FromFastBitmap(fastBitmap, 1);
+    public static IntegralImage FrommakeImage(MakeImage makeImage){
+        return FrommakeImage(makeImage, 1);
     }
     
-    /**
-     * Construct integral image from source grayscale image.
-     * @param fastBitmap Image to be processed.
-     * @param power Power of the number.
-     * @return Returns integral image.
-     */
-    public static IntegralImage FromFastBitmap(MakeImage fastBitmap, int power){
+    public static IntegralImage FrommakeImage(MakeImage makeImage, int power){
         // get source image size
-        int width  = fastBitmap.getWidth();
-        int height = fastBitmap.getHeight();
+        int width  = makeImage.getWidth();
+        int height = makeImage.getHeight();
 
         // create integral image
         IntegralImage im = new IntegralImage( width, height );
@@ -102,7 +59,7 @@ protected int[][] integralImage = null;
             int rowSum = 0;
             
             for (int j = 1; j <= width; j++) {
-                rowSum += Math.pow(fastBitmap.getGray(i - 1, j - 1), power);
+                rowSum += Math.pow(makeImage.getGray(i - 1, j - 1), power);
                 integralImage[i][j] = rowSum + integralImage[i - 1][j];
             }
         }
@@ -110,12 +67,8 @@ protected int[][] integralImage = null;
         return im;
     }
     
-    /**
-     * Process image.
-     * @param fastBitmap Image to be processed.
-     */
-    private void Process(MakeImage fastBitmap, int power){
-        if (!fastBitmap.isGrayScale()) {
+    private void Process(MakeImage makeImage, int power){
+        if (!makeImage.isGrayScale()) {
             try {
                 throw new Exception("IntegralImage works only with Grayscale images");
             } catch (Exception e) {
@@ -128,20 +81,12 @@ protected int[][] integralImage = null;
         for (int y = 1; y < height + 1; y++) {
             int rowSum = 0;
             for (int x = 1; x < width + 1; x++) {
-                rowSum += Math.pow(fastBitmap.getGray(x - 1, y - 1), power);
+                rowSum += Math.pow(makeImage.getGray(x - 1, y - 1), power);
                 integralImage[y][x] = rowSum + integralImage[y - 1][x];
             }
         }
     }
     
-    /**
-     * Calculate sum of pixels in the specified rectangle.
-     * @param x1 Coordinate of left-top rectangle's corner.
-     * @param y1 Coordinate of left-top rectangle's corner.
-     * @param x2 Coordinate of right-bottom rectangle's corner.
-     * @param y2 Coordinate of right-bottom rectangle's corner.
-     * @return Returns sum of pixels in the specified rectangle.
-     */
     public int getRectangleSum(int x1, int y1, int x2, int y2){
         // check if requested rectangle is out of the image
         if ( ( x2 < 0 ) || ( y2 < 0 ) || ( x1 >= height ) || ( y1 >= width ) )
@@ -159,24 +104,10 @@ protected int[][] integralImage = null;
         return integralImage[x2][y2] + integralImage[x1][y1] - integralImage[x1][y2] - integralImage[x2][y2];
     }
     
-    /**
-     * Calculate sum of pixels in the specified rectangle.
-     * @param x Coordinate of central point of the rectangle.
-     * @param y Coordinate of central point of the rectangle.
-     * @param radius Radius of the rectangle.
-     * @return Returns sum of pixels in the specified rectangle.
-     */
     public int getRectangleSum(int x, int y, int radius){
         return getRectangleSum(x - radius, y - radius, x + radius, y + radius);
     }
     
-    /**
-     * Calculate horizontal (X) haar wavelet at the specified point.
-     * @param x X coordinate of the point to calculate wavelet at.
-     * @param y Y coordinate of the point to calculate wavelet at.
-     * @param radius Wavelet size to calculate.
-     * @return Returns value of the horizontal wavelet at the specified point.
-     */
     public int getHarrXWavelet(int x, int y, int radius){
         int y1 = y - radius;
         int y2 = y + radius - 1;
@@ -187,13 +118,6 @@ protected int[][] integralImage = null;
         return (int) ( a - b );
     }
     
-    /**
-     * Calculate vertical (Y) haar wavelet at the specified point.
-     * @param x X coordinate of the point to calculate wavelet at.
-     * @param y Y coordinate of the point to calculate wavelet at.
-     * @param radius Wavelet size to calculate.
-     * @return Returns value of the vertical wavelet at the specified point.
-     */
     public int getHaarYWavelet( int x, int y, int radius ){
         int x1 = x - radius;
         int x2 = x + radius - 1;
@@ -204,14 +128,6 @@ protected int[][] integralImage = null;
         return (int) ( a - b );
     }
     
-    /**
-     * Calculate mean value of pixels in the specified rectangle.
-     * @param x1 X coordinate of left-top rectangle's corner.
-     * @param y1 Y coordinate of left-top rectangle's corner.
-     * @param x2 X coordinate of right-bottom rectangle's corner.
-     * @param y2 Y coordinate of right-bottom rectangle's corner.
-     * @return Returns mean value of pixels in the specified rectangle.
-     */
     public float getRectangleMean(int x1, int y1, int x2, int y2){
         // check if requested rectangle is out of the image
         if ( ( x2 < 0 ) || ( y2 < 0 ) || ( x1 >= height ) || ( y1 >= width ) )
@@ -231,25 +147,10 @@ protected int[][] integralImage = null;
             (double) ( ( x2 - x1 ) * ( y2 - y1 ) ) );
     }
     
-    /**
-     * Calculate mean value of pixels in the specified rectangle.
-     * @param x X coordinate of central point of the rectangle.
-     * @param y Y coordinate of central point of the rectangle.
-     * @param radius Radius of the rectangle.
-     * @return Returns mean value of pixels in the specified rectangle.
-     */
     public float getRectangleMean(int x, int y, int radius){
         return getRectangleMean(x - radius, y - radius, x + radius, y + radius);
     }
     
-    /**
-     * Calculate mean value of pixels in the specified rectangle without checking it's coordinates.
-     * @param x1 X coordinate of left-top rectangle's corner.
-     * @param y1 Y coordinate of left-top rectangle's corner.
-     * @param x2 X coordinate of right-bottom rectangle's corner.
-     * @param y2 Y coordinate of right-bottom rectangle's corner.
-     * @return Returns mean value of pixels in the specified rectangle.
-     */
     public int getRectangleSumUnsafe( int x1, int y1, int x2, int y2 ){
         x2++;
         y2++;
@@ -257,25 +158,10 @@ protected int[][] integralImage = null;
         return integralImage[x2][y2] + integralImage[x1][y1] - integralImage[x2][y1] - integralImage[x1][y2];
     }
     
-    /**
-     * Calculate sum of pixels in the specified rectangle without checking it's coordinates.
-     * @param x X coordinate of central point of the rectangle.
-     * @param y Y coordinate of central point of the rectangle.
-     * @param radius Radius of the rectangle.
-     * @return Returns sum of pixels in the specified rectangle.
-     */
     public int getRectangleSumUnsafe( int x, int y, int radius ){
         return getRectangleSumUnsafe( x - radius, y - radius, x + radius, y + radius );
     }
     
-    /**
-     * Calculate mean value of pixels in the specified rectangle without checking it's coordinates.
-     * @param x1 X coordinate of left-top rectangle's corner.
-     * @param y1 Y coordinate of left-top rectangle's corner.
-     * @param x2 X coordinate of right-bottom rectangle's corner.
-     * @param y2 Y coordinate of right-bottom rectangle's corner.
-     * @return Returns mean value of pixels in the specified rectangle.
-     */
     public float getRectangleMeanUnsafe( int x1, int y1, int x2, int y2 ){
         x2++;
         y2++;
@@ -285,13 +171,6 @@ protected int[][] integralImage = null;
             (double) ( ( x2 - x1 ) * ( y2 - y1 ) ) );
     }
     
-    /**
-     *  Calculate mean value of pixels in the specified rectangle without checking it's coordinates.
-     * @param x X coordinate of central point of the rectangle.
-     * @param y Y coordinate of central point of the rectangle.
-     * @param radius Radius of the rectangle.
-     * @return Returns mean value of pixels in the specified rectangle.
-     */
     public float getRectangleMeanUnsafe( int x, int y, int radius ){
         return getRectangleMeanUnsafe( x - radius, y - radius, x + radius, y + radius );
     }
